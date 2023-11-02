adba84f2e710cc145545f6712d424dd2f366ed53
cerebnismus <oguzhan.ince@protonmail.com>
2023-11-02 16:39:59 +0300

    base-system-ok

--- test.sh
+++ test.sh
@@ -1,6 +1,21 @@
+#!/bin/sh
 
+set -e
 
-DISPLAY=:0 chromium-browser --noerrdialogs --disable-session-crashed-bubble --incognito /home/raspi/magicmirror6/index.html &
+CHROMIUM_TEMP=~/tmp/chromium
+rm -Rf ~/.config/chromium/
+rm -Rf $CHROMIUM_TEMP
+mkdir -p $CHROMIUM_TEMP
+
+DISPLAY=:0 chromium-browser --noerrdialogs --disable-session-crashed-bubble --incognito --disable \
+        --disable-translate \
+        --disable-infobars \
+        --disable-suggestions-service \
+        --disable-save-password-bubble \
+        --disk-cache-dir=$CHROMIUM_TEMP/cache/ \
+        --user-data-dir=$CHROMIUM_TEMP/user_data/ \
+        --start-maximized \
+        --kiosk /home/raspi/magicmirror6/index.html &
 
 CHROME=$(echo $!)
 
@@ -10,6 +25,8 @@ sleep 5
 
 DISPLAY=:0 xdotool search --onlyvisible --class chromium-browser windowactivate --sync key F11
 
-sleep 5
+
+
+sleep 30
 
 kill -9 $CHROME
